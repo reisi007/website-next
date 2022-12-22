64f21eae3477ddf0e5d255b8fcb674033d2246a6
Florian Reisinger <reisi007@gmail.com>
2022-12-22 17:19:52 +0100

    Continue with self-made IMG tag, add .htaccess and deploy.mjs
    Use 20px blur image and add 400 to device sizes (used in img tag)

--- next.config.js
+++ next.config.js
@@ -4,23 +4,14 @@ const nextConfig = {
     swcMinify: true,
     images: {
         loader: "custom",
-        imageSizes: [400],
-        deviceSizes: [700, 1200, 2050],
+        imageSizes: [20],
+        deviceSizes: [400,700, 1200, 2050],
     },
     env: {
         nextImageExportOptimizer_imageFolderPath: "public/images",
         nextImageExportOptimizer_exportFolderPath: "out",
         nextImageExportOptimizer_quality: 50,
         nextImageExportOptimizer_storePicturesInWEBP: true,
-
-        // If you do not want to use blurry placeholder images, then you can set
-        // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-        // `placeholder="empty"` to all <ExportedImage> components.
-        //
-        // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
-        // forget to set `placeholder="empty"`, you'll see 404 errors for the missing
-        // placeholder images in the console.
-        nextImageExportOptimizer_generateAndUseBlurImages: true,
     },
 }
 
