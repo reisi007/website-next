5b24c3b39b604c1ad5ed3ca2a23635df587319e3
Florian Reisinger <reisi007@gmail.com>
2022-12-28 18:25:34 +0100

    Improve font handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 
 import { init } from '@socialgouv/matomo-next';
 import { useEffect } from 'react';
+import { FONT_MARTINA } from '../components/images-next/fonts/Font';
 
 const MATOMO_URL = 'https://analytics.reisinger.pictures/';
 const MATOMO_SITE_ID = '1';
@@ -12,7 +13,14 @@ export default function App({
   pageProps,
 }: AppProps) {
   useEffect(() => {
-    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+    init({
+      url: MATOMO_URL,
+      siteId: MATOMO_SITE_ID,
+    });
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <div className={FONT_MARTINA.variable}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
