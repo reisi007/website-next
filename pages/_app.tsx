d25b46b0a11e1ae659a7d9dfe2fe3f70942a9f84
Florian Reisinger <reisi007@gmail.com>
2023-01-18 07:18:49 +0100

    Extract Matomo component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-import { init, push } from '@socialgouv/matomo-next';
-
-import { useEffect } from 'react';
 import { CookieConsentContext } from '../components/images-next/cookies/Cookies';
 import { ConsentBanner } from '../components/images-next/cookies/ConsentBanner';
-
-const MATOMO_URL = 'https://analytics.reisinger.pictures/';
-const MATOMO_SITE_ID = '1';
+import { Matomo } from '../components/images-next/cookies/Matomo';
 
 export default function App({
   Component,
   pageProps,
 }: AppProps) {
-  useEffect(() => {
-    init({
-      url: MATOMO_URL,
-      siteId: MATOMO_SITE_ID,
-    });
-    push(['requireCookieConsent']);
-  }, []);
   return (
     <CookieConsentContext>
+      <Matomo siteId="1" />
       <ConsentBanner />
       <Component {...pageProps} />
     </CookieConsentContext>
