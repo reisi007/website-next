119c9dcd6017635456e1ae84d54866d5d5874279
Florian Reisinger <reisi007@gmail.com>
2023-01-21 08:15:20 +0100

    Improve gallery and add Matomo events

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-import { CookieConsentContext } from '../components/images-next/cookies/Cookies';
-import { ConsentBanner } from '../components/images-next/cookies/ConsentBanner';
-import { Matomo } from '../components/images-next/cookies/Matomo';
+import { CookieConsentContext } from '../components/images-next/matomo/Cookies';
+import { ConsentBanner } from '../components/images-next/matomo/ConsentBanner';
+import { Matomo } from '../components/images-next/matomo/Matomo';
 
 export default function App({
   Component,
